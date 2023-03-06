91b62df21dea32e186ed8a1756ab84376d4c703f
JayDenali26 <chesirecat.2604@gmail.com>
2023-03-06 11:44:50 -0600

    Initial Commit

---
+++ JayProject/JayProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  JayProjectApp.swift
+//  JayProject
+//
+//  Created by iOS Maquina 10 on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct JayProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
